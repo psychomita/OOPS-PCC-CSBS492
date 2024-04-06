0d2c0c227d99be3c5faf69e24d3fa9a492ae9607
Suchismita Bose <suchismitabose29@gmail.com>
2024-04-06 19:47:44 +0530

    Create ambioverload.cpp

---
+++ Ambiguous Conditions for Function Overloading/ambioverload.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+class Overload
+{
+	public :
+		Overload() {}
+		void areavol(int);
+		void areavol(long);
+		void volarea(int,int);
+		void volarea(int,int,int);
+		
+	~Overload() {}
+};
+
+void Overload :: areavol (int a)
+{
+	int area = a*a;
+	cout<<"Area of square: "<<area<<endl;
+}
+
+void Overload :: areavol (long a)
+{
+	long volume = a*a*a;
+	cout<<"Volume of cube: "<<volume<<endl;
+}
+
+void Overload :: volarea (int l, int b)
+{
+	int area = l*b;
+	cout<<"Area of rectangle: "<<area<<endl;
+}
+
+void Overload :: volarea (int l, int b, int h)
+{
+	int volume = l*b*h;
+	cout<<"Volume of cuboid: "<<volume<<endl;
+}
+
+int main()
+{
+	int a;
+	float c;
+	Overload obj;
+	cout<<"Enter a float value: ";
+	cin>>c;
+	obj.areavol(c);
+	cout<<"Enter a integer value: ";
+	cin>>a;
+	obj.volarea(a);
+	return 0;
+}
